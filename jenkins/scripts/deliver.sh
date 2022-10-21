74f5bbb204a6147f8fdfb193f52b6206a5aba464
gschipper <gschipper@gmail.com>
2022-10-21 11:13:09 +0200

    taking artifact id instead of name to construct deliberable according to name of generated jar

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -8,10 +8,10 @@ set -x
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 set +x
 
-echo 'The following complex command extracts the value of the <name/> element'
+echo 'The following complex command extracts the value of the <artifactId/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
-NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
+ARTIFACT=`mvn help:evaluate -Dexpression=project.artifactId | grep "^[^\[]"`
 set +x
 
 echo 'The following complex command behaves similarly to the previous one but'
@@ -23,4 +23,4 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/${NAME}-${VERSION}.jar
+java -jar target/${ARTIFACT}-${VERSION}.jar
